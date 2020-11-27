fd94e867c1d6133f47efd966b5a13a6d4be39a3a
joshua kim <j0shxone@gmail.com>
2020-11-27 00:29:40 -0500

    added seed file, browsing capability.

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -1,1 +1,13 @@
-browse_controller.rb
+class BrowseController < ApplicationController
+
+  def browse
+    @users = Account.where.not(id: current_account.id)
+  end
+
+  def approve
+  end
+
+  def decline
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Account.create([
+  { first_name: "Christine", last_name: "Jishi", username: "christinejishi", email: "christine@gmail.com", password:"Password1"},
+  { first_name: "Emily", last_name: "Lee", username: "emilylee", email: "emilylee@gmail.com", password:"Password1"},
+  { first_name: "Melanie", last_name: "Kim", username: "mkim", email: "mkim@gmail.com", password:"Password1"},
+  { first_name: "Carol", last_name: "Baskin", username: "carolbaskin", email: "carolbaskin@gmail.com", password:"Password1"},
+  { first_name: "Bridget", last_name: "Choi", username: "bridgetc", email: "bridgetc@gmail.com", password:"Password1"},
+])
