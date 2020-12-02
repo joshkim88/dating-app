6d7e8c9b1b3438153615403f3ae52cc7afcca28f
joshua kim <j0shxone@gmail.com>
2020-12-02 14:30:39 -0500

    fixed sign out button bug.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ require("bootstrap")
 require("../includes/slides.js")
 require("@fortawesome/fontawesome-free";
 
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
