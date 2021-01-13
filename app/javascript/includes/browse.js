1910c4c31902e06f55730d8d6e4e45dd07a8b4c2
joshua kim <j0shxone@gmail.com>
2021-01-12 19:23:09 -0500

    fixed another browsing bug by adding a default display none to conversation scss.

--- app/javascript/includes/browse.js
+++ app/javascript/includes/browse.js
@@ -1,9 +1,6 @@
 $(function(){
   var $activeSlide = $('#slides .slide:first-child');
 
-  // show first slide
-  // $activeSlide.addClass("showing");
-
   $(".match-tile").on("click", function(){
     var account_id = $(this).data("id");
 
